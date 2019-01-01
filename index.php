0ab6fccca949bd9fbfd02260190513f51306015d
OHLECK <sebastien@ohleck.fr>
2019-01-01 23:30:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-
+session_start();
 include 'clientLog.php';
 clientLog("index");
 include 'html.php';
 
-//cleaning operation session
+//clean flush operation for debug and test
 //include 'destroy.php';
 
 ?>
