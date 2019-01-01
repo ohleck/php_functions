c7ebe3b77c69a87eb242d2c86602f252692a5618
OHLECK <sebastien@ohleck.fr>
2019-01-01 23:27:28 +0100

    Update html.php

--- html.php
+++ html.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8">
-    <style>
+<title>ABC-GAMING</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
 
-    </style>
-    
-    </style>
+</style>
+
 <script src="/js/map.js" type="text/javascript"></script>
 <script>
     var longitude = "<?php echo $ip_longitude; ?>";
