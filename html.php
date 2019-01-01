ae68e16212c91500ccc36e5c4a9772f02ef7ad05
OHLECK <sebastien@ohleck.fr>
2019-01-01 21:55:52 +0100

    Create html.php

---
+++ html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <style>
+
+    </style>
+</head>
+<body>
+
+</body>
+</html>
